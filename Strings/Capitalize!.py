ea2a8f6ec2c84406eb52520fa23405e1659e2b5f
Madhav Arora <115910980+Madhavarora05@users.noreply.github.com>
2023-06-15 00:43:35 +0530

    Strings - Python Challenge solutions

---
+++ Strings/Capitalize!.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    ans =""
+    l = s.split(" ")
+    for i in l:
+        if i.isdigit():
+            ans += i +" "
+        else:
+            ans+= i.capitalize()+" "
+    return ans.strip()
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
