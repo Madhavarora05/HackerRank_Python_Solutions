a7281c9983c9c00118933147905dc6c6b4050b98
Madhav Arora <115910980+Madhavarora05@users.noreply.github.com>
2023-06-18 19:50:39 +0530

    Sets - Python Challenge solutions

--- Symmetric Difference.py
+++
@@ -1,8 +0,0 @@
-M=int(input())
-a=set(map(int,input().split()))
-N=int(input())
-b=set(map(int,input().split()))
-a_diff = a.difference(b)
-b_diff = b.difference(a)
-for i in sorted(a_diff.union(b_diff)):
-    print(i)
